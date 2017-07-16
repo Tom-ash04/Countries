a08eb524b10031c57b7feb8c3fdb524c749e8a2d
Tom-ash04 <tomy04@vp.pl>
2017-07-16 13:36:31 +0200

    update css and js

--- country.js
+++ country.js
@@ -7,7 +7,7 @@ function searchCountries(){
     var countryName = $("#country-name").val();
     if(!countryName.length) countryName = "Poland";
     $.ajax({
-        url: url + countryName,
+        url: url + countryName, 
         method: "GET",
         success: showCountriesList,
     });
@@ -16,9 +16,41 @@ function searchCountries(){
 function showCountriesList(resp){
     countriesList.empty();
     resp.forEach(function(item){
-        var $text = $("<p>").text(item.name + " , Capital: " + item.capital + " , Region: " + item.subregion);
+        var $container = $("<div>").addClass("container");
+        var $flagcont = $("<div>").addClass("flag");
         var $flag = $("<img>").attr({src : item.flag});
-        $("<li>").append($flag).append($text).appendTo(countriesList);
+        var $namecont = $("<div>").addClass("name");
+        var $name = $("<h1>").text(item.name);
+        var $capitalcont = $("<div>").addClass("capital");
+        var $capitalinfo = $("<strong>").text("Capital:");
+        var $capital = $("<p>").text(item.capital);
+        var $regioncont = $("<div>").addClass("region");
+        var $regioninfo = $("<strong>").text("Region:");
+        var $region = $("<p>").text(item.subregion);
+        var $phonecont = $("<div>").addClass("phone");
+        var $phoneinfo = $("<strong>").text("Phone calling code:");
+        var $phone = $("<p>").text("+ " + item.callingCodes);
+        var $currencycont = $("<div>").addClass("currency");
+        var $currencyinfo = $("<strong>").text("Currency:");
+        var $currency = $("<p>").text(item.currencies[0].code);
+        var $populationcont = $("<div>").addClass("population");
+        var $populationinfo = $("<strong>").text("Population:");
+        var $population = $("<p>").text(item.population);
+        var $domaincont = $("<div>").addClass("domain");
+        var $domaininfo = $("<strong>").text("Domain:");
+        var $domain = $("<p>").text(item.topLevelDomain[0]);
+        $flagcont.append($flag).appendTo($container);
+        $namecont.append($name).appendTo($container);
+        $capitalcont.append($capitalinfo).append($capital).appendTo($container);
+        $regioncont.append($regioninfo).append($region).appendTo($container);
+        $phonecont.append($phoneinfo).append($phone).appendTo($container);
+        $currencycont.append($currencyinfo).append($currency).appendTo($container);
+        $populationcont.append($populationinfo).append($population).appendTo($container);
+        $domaincont.append($domaininfo).append($domain).appendTo($container);
+        $("<li>").append($container).appendTo(countriesList);
+        // $("<li>").append($name).appendTo(countriesList);
+        // $("<li>").append($capital).appendTo(countriesList);
+        // $("<li>").append($phone).appendTo(countriesList);
     });
 }
 
